1fdf77f742054dd7a4b14576d6c220e17afbf6b4
AdryanMaikel <adryanmaikel@gmail.com>
2023-08-18 15:20:39 -0300

    completed salário com bonus

--- salário com bônus/README.md
+++ salário com bônus/README.md
@@ -1,3 +1,4 @@
+## 
 Faça um programa que leia o nome de um vendedor, o seu salário fixo e o total de vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15% de comissão sobre suas vendas efetuadas, informar o total a receber no final do mês, com duas casas decimais.
 
 ## Entrada
--- salário com bônus/salario_com_bonus.rb
+++ salário com bônus/salario_com_bonus.rb
@@ -1,1 +1,21 @@
-name = ""
+class Seller
+  def initialize name, salary_fixed, sales_made
+    @name = name
+    @salary_fixed = salary_fixed
+    @sales_made = sales_made
+  end
+
+  def calculate_salary_with_bonus
+    @sales_made * 0.15 + @salary_fixed
+  end
+
+  def to_s
+    puts "TOTAL = R$ #{format("%.2f", calculate_salary_with_bonus)}"
+  end
+end
+
+name = gets.chomp
+salary_fixed = gets.chomp.to_f
+sales_made = gets.chomp.to_f
+
+Seller.new(name, salary_fixed, sales_made).to_s
