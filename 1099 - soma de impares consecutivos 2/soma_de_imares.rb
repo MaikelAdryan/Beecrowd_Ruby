72a22a889a9f654532777e196d55702956d41b51
AdryanMaikel <adryanmaikel@gmail.com>
2023-08-29 14:59:20 -0300

    completo

--- 1099 - soma de impares consecutivos 2/soma_de_imares.rb
+++ 1099 - soma de impares consecutivos 2/soma_de_imares.rb
@@ -1,4 +1,17 @@
-gets.chomp.to_i.times{
+gets.chomp.to_i.times{ |a, b, total|
+  total = 0
   a, b = gets.chomp.split(" ").map(&:to_i)
-  (a..b).map{|i| p i}
+  if a < b
+    while a < b
+      a += 1
+      total += a if a != b and a.odd?
+    end
+    puts total
+  else
+    while b < a
+      b += 1
+      total += b if b != a and b.odd?
+    end
+    puts total
+  end
 }
